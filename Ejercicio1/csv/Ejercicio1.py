901470be01b7600d10eefed5eebccec4b5e9a98a
tereesaalvarez <93209324+tereesaalvarez@users.noreply.github.com>
2023-11-07 23:46:20 +0000

    commit

--- Ejercicio1/csv/Ejercicio1.py
+++ Ejercicio1/csv/Ejercicio1.py
@@ -1,12 +1,11 @@
 import pandas as pd
-import seaborn as sns
-
 
 data = pd.read_csv('Ejercicio1/csv/activaciones.csv', delimiter=';', encoding='ISO-8859-1')
 
-#data= data.dropna(axis=1, how='all')
+columnas_eliminar = ['PRECIO','DIAS-EXCLUIDOS','DESCRIPCION','CONTENT-URL','URL-ACTIVIDAD','URL-INSTALACION' ]
+
+data= data.drop(columnas_eliminar, axis=1)
 
 
-
-#data.to_csv('Ejercicio1/csv/activaciones1.csv', index=False)
+data.to_csv('Ejercicio1/csv/activaciones1.csv', index=False)
 
