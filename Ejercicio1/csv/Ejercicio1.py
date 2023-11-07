577928542353f6d1fded312418ec1880a638d929
tereesaalvarez <alvdeporteresa@gmail.com>
2023-11-08 00:09:34 +0100

    commit

--- Ejercicio1/csv/Ejercicio1.py
+++ Ejercicio1/csv/Ejercicio1.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import seaborn as sns
+
 
 data = pd.read_csv('Ejercicio1/csv/activaciones.csv', delimiter=';', encoding='ISO-8859-1')
 
-data= data.dropna(axis=1, how='all')
-data = data.dropna(thresh=len(data.columns) - 4)
+#data= data.dropna(axis=1, how='all')
 
 
-data.to_csv('Ejercicio1/csv/activaciones1.csv', index=False)
+
+#data.to_csv('Ejercicio1/csv/activaciones1.csv', index=False)
 
