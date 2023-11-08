42433ac06275f21ad182e17c5209506ca083845d
tereesaalvarez <alvdeporteresa@gmail.com>
2023-11-09 00:05:06 +0100

    pizza especifica

---
+++ Ejercicio2/Pizzas especificas/margarita.py
@@ -0,0 +1,39 @@
+#pizzabuilder para construir pizzas especificas
+from Ejercicio2.prueba import *
+from Ejercicio2.pizza import *
+
+class MargaritaBuilder(PizzaBuilder):
+
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._pizza = Pizza()
+
+    @property
+    def pizza(self) -> Pizza:
+        pizza = self._pizza
+        self.reset()
+        return pizza
+
+    def producir_masa(self) -> None:
+        self._pizza.masa = 'masa fina'
+
+    def producir_salsa(self) -> None:
+        self._pizza.salsa = 'salsa de tomate'
+
+    def producir_ingredientes(self) -> None:
+        self._pizza.ingredientes = 'mozzarella, albahaca'
+
+    def producir_coccion(self) -> None:
+        self._pizza.coccion = 'horno tradicional'
+
+    def producir_presentacion(self) -> None:
+        self._pizza.presentacion = 'pizza circular'
+
+    def producir_maridaje(self) -> None:
+        self._pizza.maridaje = 'cerveza rubia'
+
+    def producir_extras(self) -> None:
+        self._pizza.extras = 'aceitunas negras'
+
--- Ejercicio2/prueba.py
+++ Ejercicio2/prueba.py
@@ -39,29 +39,6 @@ class PizzaBuilder(ABC):
         pass
 
 
-class ConcreteBuilder1(Builder):
-
-    def __init__(self) -> None:
-        self.reset()
-
-    def reset(self) -> None:
-        self._product = Product1()
-
-    @property
-    def product(self) -> Product1:
-        product = self._product
-        self.reset()
-        return product
-
-    def produce_part_a(self) -> None:
-        self._product.add("PartA1")
-
-    def produce_part_b(self) -> None:
-        self._product.add("PartB1")
-
-    def produce_part_c(self) -> None:
-        self._product.add("PartC1")
-
 
 
 
