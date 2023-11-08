9d643756addce1dda731605423edba9be5f1220f
tereesaalvarez <alvdeporteresa@gmail.com>
2023-11-09 00:29:32 +0100

    hawaiana

---
+++ Ejercicio2/Pizzas especificas/hawaiana.py
@@ -0,0 +1,38 @@
+from Ejercicio2.prueba import *
+from Ejercicio2.pizza import *
+
+#deberia de estar prohibida
+class HawaianaBuilder(PizzaBuilder):
+
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._pizza = Pizza()
+
+    @property
+    def pizza(self) -> Pizza:
+        pizza = self._pizza
+        self.reset()
+        return pizza
+
+    def producir_masa(self) -> None:
+        self._pizza.masa = 'masa fina'
+
+    def producir_salsa(self) -> None:
+        self._pizza.salsa = 'salsa de tomate'
+
+    def producir_ingredientes(self) -> None:
+        self._pizza.ingredientes = 'jamón, piña, queso mozzarella'
+
+    def producir_coccion(self) -> None:
+        self._pizza.coccion = 'horno tradicional'
+
+    def producir_presentacion(self) -> None:
+        self._pizza.presentacion = 'pizza circular'
+
+    def producir_maridaje(self) -> None:
+        self._pizza.maridaje = 'zumo de piña'
+
+    def producir_extras(self) -> None:
+        self._pizza.extras = 'cerezas en almíbar'
