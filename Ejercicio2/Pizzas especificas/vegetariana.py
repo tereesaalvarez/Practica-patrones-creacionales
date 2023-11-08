e91a333551f43c9105cc5f0f4895ba77019ca6ba
tereesaalvarez <alvdeporteresa@gmail.com>
2023-11-09 00:33:05 +0100

    vegetariana

---
+++ Ejercicio2/Pizzas especificas/vegetariana.py
@@ -0,0 +1,37 @@
+from Ejercicio2.prueba import *
+from Ejercicio2.pizza import *
+
+class VegetarianaBuilder(PizzaBuilder):
+
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._pizza = Pizza()
+
+    @property
+    def pizza(self) -> Pizza:
+        pizza = self._pizza
+        self.reset()
+        return pizza
+
+    def producir_masa(self) -> None:
+        self._pizza.masa = 'masa fina'
+
+    def producir_salsa(self) -> None:
+        self._pizza.salsa = 'salsa de tomate'
+
+    def producir_ingredientes(self) -> None:
+        self._pizza.ingredientes = 'tomate, champiñones, pimientos, cebolla, aceitunas negras'
+
+    def producir_coccion(self) -> None:
+        self._pizza.coccion = 'horno tradicional'
+
+    def producir_presentacion(self) -> None:
+        self._pizza.presentacion = 'pizza circular'
+
+    def producir_maridaje(self) -> None:
+        self._pizza.maridaje = 'vino blanco o refresco'
+
+    def producir_extras(self) -> None:
+        self._pizza.extras = 'albahaca fresca y queso parmesano'
