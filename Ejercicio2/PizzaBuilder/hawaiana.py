d42f2f048f2b64b31e2b22a2522c8c7cb7e79436
tereesaalvarez <alvdeporteresa@gmail.com>
2023-11-09 11:08:54 +0100

    Ahora ya entiendo

--- Ejercicio2/PizzaBuilder/hawaiana.py
+++ Ejercicio2/PizzaBuilder/hawaiana.py
@@ -36,3 +36,23 @@ class HawaianaBuilder(PizzaBuilder):
 
     def producir_extras(self) -> None:
         self._pizza.extras = 'cerezas en almíbar'
+
+
+class Product1():
+    """
+    It makes sense to use the Builder pattern only when your products are quite
+    complex and require extensive configuration.
+
+    Unlike in other creational patterns, different concrete builders can produce
+    unrelated products. In other words, results of various builders may not
+    always follow the same interface.
+    """
+
+    def __init__(self) -> None:
+        self.parts = []
+
+    def add(self, part: Any) -> None:
+        self.parts.append(part)
+
+    def list_parts(self) -> None:
+        print(f"Product parts: {', '.join(self.parts)}", end="")
